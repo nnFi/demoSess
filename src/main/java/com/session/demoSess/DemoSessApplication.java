872cfb156403b58060071feee83eddc9965318cd
Finn Plassmeier <plassmeier@volavis.de>
2025-03-25 07:05:19 +0100

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/session/demoSess/DemoSessApplication.java
@@ -0,0 +1,13 @@
+package com.session.demoSess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/session/demoSess/DemoSessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.session.demoSess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
